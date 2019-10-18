8cac1bfb071e6d4a510096dade397515af044a34
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-10-18 12:12:55 +0530

    Express setup

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 const http = require("http");
-const routes = require('./routes');
 
-// Setting a port
 const PORT = 3000;
-
-
-    //Displying basic details of Request
-
-    // console.log(
-    //     `
-    //     url: ${req.url}
-    //     headers: ${req.headers}
-    //     method: ${req.method}
-    //     `
-    //     );
-
-    //To hard exit -demo purposes only
-    //process.exit();
     
 const server = http.createServer(routes);
 
